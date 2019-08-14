8667d7927fc9ca0ae61731f5ccdd7b30e8e57319
YukJiSoo <wltn3231@naver.com>
2019-08-14 17:26:12 +0900

    [Model] District 모델 구현 및 asset 추가

---
+++ Umchan/Model/DistrictCoordinate.swift
@@ -0,0 +1,15 @@
+//
+//  District.swift
+//  Umchan
+//
+//  Created by 육지수 on 8/14/19.
+//  Copyright © 2019 JSYuk. All rights reserved.
+//
+
+import Foundation
+
+struct DistrictCoordinate: Codable {
+    var name: String?
+    var x: Float?
+    var y: Float?
+}
