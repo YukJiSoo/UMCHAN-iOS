8bbc5ddaa23ea1e5f8e5f0c50ea7728156de35de
YukJiSoo <wltn3231@naver.com>
2019-08-16 23:42:24 +0900

    [Custom] Custom Navigation Bar 구현

--- Umchan/Views/CustomNavigationBar.swift
+++ Umchan/Views/CustomNavigationBar.swift
@@ -11,19 +11,68 @@ import UIKit
 @IBDesignable
 class CustomNavigationBar: UIView {
     
+    // MARK: - Properties
+    var titleLabel: UILabel!
+    var rightButton: UIButton?
+    var leftButton: UIButton?
+    
+    @IBInspectable var barItemSize: CGFloat = 30
+    @IBInspectable var padding: CGFloat = 15
+    
+    // MARK: - Life cycles
     override init(frame: CGRect) {
         super.init(frame: frame)
+        print("1")
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+//        self.setupViews()
+        print("2")
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.setupViews()
+        print("3")
     }
     
     override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
+    }
+    
+    // MARK: - Functions
+    func setupViews() {
+        
+        self.titleLabel = UILabel(frame: CGRect.zero)
+        self.titleLabel.text = "움찬러너"
+        self.titleLabel.font = UIFont(name: "NanumSquareOTF_ac", size: CGFloat(20))
+        self.titleLabel.textColor = SYMBOL_COLOR
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.rightButton = UIButton(frame: CGRect.zero)
+        self.rightButton?.setImage(UIImage(named: "user"), for: .normal)
+        self.rightButton?.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(self.titleLabel)
+        if let rightButton = self.rightButton {
+            self.addSubview(rightButton)
+        }
+        
+        self.setupConstraints()
+    }
+    
+    func setupConstraints() {
+        
+        self.titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 20).isActive = true
+        
+        if let rightButtonTrailingAnchor = self.rightButton?.trailingAnchor {
+            self.trailingAnchor.constraint(equalTo: rightButtonTrailingAnchor, constant: self.padding).isActive = true
+        }
+        self.rightButton?.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 20).isActive = true
+        self.rightButton?.widthAnchor.constraint(equalToConstant: barItemSize).isActive = true
+        self.rightButton?.heightAnchor.constraint(equalToConstant: barItemSize).isActive = true
+        
     }
 }
